d818859c0dac55129dec5e7d207127b65e262db9
thetala3 <talamohammed2000@gmail.com>
2026-03-17 23:16:43 +0300

    Add domain entities

---
+++ CarExplorer.Domain/Entities/Make.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarExplorer.Domain.Entities
+{
+    public class Make
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ CarExplorer.Domain/Entities/Model.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarExplorer.Domain.Entities
+{
+    public class Model
+    {
+        public string Name { get; set; }
+    }
+}
