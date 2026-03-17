b69aac78966b95aadc65c989e5cd031e1e36b056
thetala3 <talamohammed2000@gmail.com>
2026-03-17 23:19:47 +0300

    Add IvehicleService interface

---
+++ CarExplorer.Application/Interfaces/IvehicleService.cs
@@ -0,0 +1,14 @@
+﻿using CarExplorer.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarExplorer.Application.Interfaces
+{
+    public interface IvehicleService
+    {
+        Task<IEnumerable<Make>> GetMakesAsync();
+        Task<IEnumerable<VehicleType>> GetVehicleTypesAsync(int makeId);
+        Task<IEnumerable<Model>> GetModelsAsync(int makeId, int year);
+    }
+}
