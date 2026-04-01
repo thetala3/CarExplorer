16469faea02a8340cfef5364e19e0dec8582a2bf
thetala3 <talamohammed2000@gmail.com>
2026-04-01 14:42:41 +0300

    API Controller

---
+++ CarExplorer.API/Controllers/VehiclesController.cs
@@ -0,0 +1,39 @@
+﻿using CarExplorer.Application.Interfaces;
+using CarExplorer.Infrastructure.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarExplorer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IvehicleService _service;
+        public VehiclesController(IvehicleService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("makes")]
+        public async Task<IActionResult> GetMakes()
+        {
+            var result = await _service.GetMakesAsync();
+            return Ok(result);
+        }
+
+        [HttpGet("vehicle-types")]
+        public async Task<IActionResult> GetVehicleTypes(int makeId)
+        {
+            var result = await _service.GetVehicleTypesAsync(makeId);
+            return Ok(result);
+        }
+
+        [HttpGet("models")]
+        public async Task<IActionResult> GetModels(int makeId, int year)
+        {
+            var result = await _service.GetModelsAsync(makeId, year);
+            return Ok(result);
+        }
+    }
+}
